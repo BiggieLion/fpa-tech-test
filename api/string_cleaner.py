67e29f490c74cc2bb22e365a7b8cc2aee67c5bca
Jair León <hleonr1300@alumno.ipn.mx>
2024-05-10 15:44:26 -0600

    chore(project): add response.json file as example, add comments to documentate all the functions of the project following the Google Standard

--- api/string_cleaner.py
+++ api/string_cleaner.py
@@ -3,31 +3,114 @@ import re
 
 
 def remove_extra_curly_bracket(string):
+    """
+    This function removes the extra curly bracket at the end of the string
+
+    Args:
+        - string: The raw string to be cleaned from the text vars
+
+    Returns:
+        - string: The cleaned string
+
+    Raises:
+        - Internal Error: If the string does not have an extra curly bracket
+    """
     return string[:-1]
 
 
 def remove_data_field(string):
+    """
+    This function removes the data field from the string
+
+    Args:
+        - string: The raw string to be cleaned from the text vars
+
+    Returns:
+        - string: The cleaned string
+
+    Raises:
+        - Internal Error: If the string does not have a data field
+    """
     return string.split('data": ')[1]
 
 
 def remove_nones(string):
+    """
+    This function removes the None values from the string
+
+    Args:
+        - string: The raw string to be cleaned from the text vars
+
+    Returns:
+        - string: The cleaned string
+
+    Raises:
+        - Internal Error: If the string does not have a None value
+    """
     return string.replace("None", '""')
 
 
 def remove_rn(string):
+    """
+    This function removes the \r and \n from the string
+
+    Args:
+        - string: The raw string to be cleaned from the text vars
+
+    Returns:
+        - string: The cleaned string
+
+    Raises:
+        - Internal Error: If the string does not have a \r or \n
+    """
     return re.sub(r"\r*\n\r*", "", string)
 
 
 def remove_before_curly_bracket(string):
+    """
+    This function removes the text before the first curly bracket that is not the JSON Object
+
+    Args:
+        - string: The raw string to be cleaned from the text vars that is not the JSON Object
+
+    Returns:
+        - string: The cleaned string
+
+    Raises:
+        - None
+    """
     return re.split("^[^{]*{", string)[1]
 
 
 def remove_after_curly_bracket(string):
+    """
+    This function removes the text after the last curly bracket that is not the JSON Object
+
+    Args:
+        - string: The raw string to be cleaned from the text vars that is not the JSON Object
+
+    Returns:
+        - string: The cleaned string
+
+    Raises:
+        - None
+    """
     pre_cleaned = string.rsplit("}", 1)[0]
     return pre_cleaned.replace(" ", "")
 
 
 def clean_string(string):
+    """
+    This is the main function that cleans all the raw text to be parsed as a JSON Object
+
+    Args:
+        - string: The raw string to be cleaned from the text vars
+
+    Returns:
+        - string: The cleaned string
+
+    Raises:
+    """
     cleaned = remove_data_field(string)
     cleaned = remove_rn(cleaned)
     cleaned = remove_extra_curly_bracket(cleaned)
